97563da58479ca89cae5a9a3df71d65a7ba60f6c
Akash-minfy <akash.kumar@minfytech.com>
2025-06-23 01:49:07 +0530

    Implement open API spec

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/user.routes');
+
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs');
+const path = require('path');
+
 const cors = require('cors');
 dotenv.config();
 
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 3000;
+const swaggerDocument = YAML.load(path.join(__dirname, './docs/openapi.yaml'));
+
 
 app.use(express.json());
 
@@ -23,6 +30,10 @@ app.get('/', (req, res) => {
 // Use our user routes for any path starting with /api
 app.use('/api', userRoutes);
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// Server start
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Swagger docs available at http://localhost:${PORT}/api-docs`);
 });
